3564730a968ff1a7211864cb5b8a983cdf95b117
austindickey <austindickey@me.com>
2020-03-10 21:01:47 -0500

    updating product list in schema file

--- schema.sql
+++ schema.sql
@@ -16,16 +16,16 @@ CREATE TABLE products(
 SELECT * FROM products;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Uncharted 4", "Video Games", 49.95, 150),
-  ("DOOM", "Video Games", 59.99, 200),
-  ("Crate of Spam", "Food and Drink", 24.50, 50),
-  ("Cool Shades", "Apparel", 75.00, 5),
-  ("Worn Denim Jeans", "Apparel", 54.25, 35),
-  ("Survival Towel", "Necessities", 42.42, 42),
-  ("Bill and Ted's Excellent Adventure", "Films", 15.00, 25),
-  ("Mad Max: Fury Road", "Films", 25.50, 57),
+VALUES ("Red Dead Redemption 2", "Video Games", 59.99, 150),
+  ("2K20", "Video Games", 59.99, 140),
+  ("24 Pack of Dr. Pepper", "Food and Drink", 7.50, 50),
+  ("Oakley Sunglasses", "Apparel", 125.00, 9),
+  ("Lucky Jeans", "Apparel", 78.25, 35),
+  ("Survival Kit", "Necessities", 38.42, 42),
+  ("Ted 2", "Films", 12.00, 25),
+  ("Step Brothers", "Films", 9.50, 57),
   ("Monopoly", "Board Games", 30.50, 35),
-  ("Yahtzee", "Board Games", 19.95, 23);
+  ("Risk", "Board Games", 19.95, 23);
 
  CREATE TABLE departments(
   department_id INT AUTO_INCREMENT NOT NULL,
