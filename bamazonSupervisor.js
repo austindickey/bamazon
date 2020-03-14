d63875e01433cd11d78b93b7382f77354d76c35e
austindickey <austindickey@me.com>
2020-03-14 15:48:42 -0500

    added calculations for product sales and total profit and display them in the console log

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,7 +1,9 @@
+// No starter code for this file
+
 // Initializes the npm packages used
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-require("console.table");
+var mysql = require("mysql")
+var inquirer = require("inquirer")
+require("console.table")
 
 // Initializes the connection variable to sync with a MySQL database
 var connection = mysql.createConnection({
@@ -16,61 +18,131 @@ var connection = mysql.createConnection({
   // Your password
   password: "root",
   database: "bamazon"
-});
+})
 
 // Creates the connection with the server and loads the manager menu upon a successful connection
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
   }
-  loadSupervisorMenu();
-});
+  loadSupervisorOptions()
+})
 
-// Get product data from the database
-function loadSupervisorMenu() {
+function getDepartments() {
+  var departmentNameArray = []
+
+  connection.query("SELECT * FROM products", function(err, res){
+    if (err) throw err
+
+    for (var i = 0; i < res.length; i++) {
+      if (departmentNameArray.indexOf(res[i].department_name) > -1) {
+        
+      } else {
+        departmentNameArray.push(res[i].department_name)
+      }
+    }
+
+    calculateSales(departmentNameArray)
+  })
+}
+
+function calculateSales(departmentNameArray) {
+  for (let i = 0; i < departmentNameArray.length; i++) {
+    connection.query("SELECT product_sales FROM products WHERE department_name = '" + departmentNameArray[i] + "'", function(err, res) {
+      if (err) throw err
+      for (let x = 0; x < res.length; x++) {
+        connection.query("UPDATE departments SET product_sales = product_sales + " + res[x].product_sales + " WHERE department_name = '" + departmentNameArray[i] + "'", function(err, res) {
+          if (err) throw err
+        })
+      }
+    })
+  }
+
+  calculateProfit(departmentNameArray)
+}
+
+function calculateProfit(departmentNameArray) {
+  for (let i = 0; i < departmentNameArray.length; i++) {
+    connection.query("SELECT over_head_costs, product_sales FROM departments WHERE department_name = '" + departmentNameArray[i] + "'", function(err, res) {
+      if (err) throw err
+      
+      for (let x = 0; x < res.length; x++) {
+        var profit = res[x].product_sales - res[x].over_head_costs 
+        connection.query("UPDATE departments SET total_profit = " + profit + " WHERE department_name = '" + departmentNameArray[i] + "'", function(err, res) {
+          if (err) throw err
+        })
+      }
+
+    })
+  }
+
+  loadDepartmentData()
+}
+
+function loadDepartmentData() {
     connection.query("SELECT * FROM departments", function(err, res) {
-      if (err) throw err;
+      if (err) throw err
+      console.log("\n")
+      console.table(res)
+
+      loadSupervisorOptions()
+    })
+}
   
-      // Load the possible manager menu options, pass in the products data
-      loadSupervisorOptions(res);
-    });
-  }
-  
-  // Load the manager options and pass in the products data from the database
-  function loadSupervisorOptions(departments) {
-    inquirer
-      .prompt({
-        type: "list",
-        name: "choice",
-        choices: ["View Product Sales by Department", "Create New Department", "Quit"],
-        message: "\nWhat would you like to do?\n"
-      })
-      .then(function(val) {
+function loadSupervisorOptions() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "choice",
+      choices: ["View Product Sales by Department", "Create New Department", "Quit"],
+      message: "\nWhat would you like to do?\n"
+    })
+    .then(function(val) {
+      
+      switch (val.choice) {
+        case "View Product Sales by Department":
+          getDepartments()
+          loadSupervisorOptions()
+
+          break;
+      
+        case "Create New Department":
+          newDepartment()
         
-        switch (val.choice) {
-          case "View Product Sales by Department":
-            console.log("\n")
-            console.table(departments)
-            loadSupervisorOptions(products)
-  
-            break;
-        
-          case "Create New Department":
-            newDepartment()
-          
-            break;
-  
-          case "Quit":
-            console.log("\nGoodbye!\n")
-            process.exit(0)
-  
-            break;
-        }
-  
-      });
-  }
+          break;
+
+        case "Quit":
+          console.log("\nGoodbye!\n")
+          process.exit(0)
+
+          break;
+      }
+
+    })
+}
 
 function newDepartment() {
-  // still need to code
+  inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "department_name",
+          message: "What is the name of the department you would like to add?"
+        },
+        {
+          type: "input",
+          name: "over_head_costs",
+          message: "What is the over head cost of this department?"
+        }
+      ])
+      .then(function(val) {
+        
+        connection.query("INSERT INTO departments (department_name, over_head_costs) VALUES (" + val.department_name + ", " + val.over_head_costs + ")", function(err, res){
+          if (err) throw err
+
+          // probably just a query call to push data to the table
+        })
   
+      })
+
 }
